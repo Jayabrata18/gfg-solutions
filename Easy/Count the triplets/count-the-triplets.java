f9ea66860e33f3b98be8ffe9ba794baa8385ac19
Jayabrata Pramanik <jayabrata180402@gmail.com>
2024-04-22 22:49:25 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count the triplets/count-the-triplets.java
+++ Easy/Count the triplets/count-the-triplets.java
@@ -33,18 +33,18 @@ class GFG
 class Solution {
     int countTriplet(int arr[], int n) {
         // code here
-          Arrays.sort(arr);
+        Arrays.sort(arr);
         int count=0;
         for(int i=0;i<n;i++){
         int first=0;
         int last=n-1;
 
-        
         while(first<last){
             if(arr[first]+arr[last]==arr[i]){
                 count++;
                 first++;
                 last--;
+                
             }else if(arr[first]+arr[last]>arr[i]){
                 last--;
             }else{

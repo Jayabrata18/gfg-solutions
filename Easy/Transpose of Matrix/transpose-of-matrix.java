69d01a659dd7b823218fbd71cdf30ab537cc72bd
Jayabrata Pramanik <jayabrata180402@gmail.com>
2024-04-25 13:43:20 +0530

    Updated Solution - GfG to GitHub

--- Easy/Transpose of Matrix/transpose-of-matrix.java
+++ Easy/Transpose of Matrix/transpose-of-matrix.java
@@ -41,7 +41,7 @@ class Solution
     public void transpose(int n,int a[][])
     {
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=i;j<n;j++){
                 int temp =a[i][j];
                 a[i][j]=a[j][i];
                 a[j][i]=temp;
